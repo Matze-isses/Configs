f83884e2f9b10bcfa02bb3283bc2910574241ec5
matze-isses <ehrhardt.matthis@gmail.com>
2024-07-28 09:11:49 +0200

    improved integration of shell skript
    Signed-off-by: matze-isses <ehrhardt.matthis@gmail.com>

--- config_switcher.sh
+++ config_switcher.sh
@@ -10,71 +10,13 @@ create_symlink() {
 
 
 case $1 in
-    -hm)
-        create_symlink "$HOME/configs/hyprland/hyprland_master.conf" "$HOME/.config/hypr/hyprland.conf"
-        ;;
-    -h)
-        read -p "-> Possibilities: (1: [PC] | 2: Laptop): " user_input
-        case $user_input in
-            "PC")
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                ;;
-            "1")
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                ;;
-            "Laptop")
-                create_symlink "$HOME/configs/hyprland/hyprland_laptop.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper_laptop.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                ;;
-            "2")
-                create_symlink "$HOME/configs/hyprland/hyprland_laptop.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper_laptop.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                ;;
-            *)
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                ;;
-        esac
-        ;;
-    -a)
-        read -p "-> Possibilities: (1: [PC] | 2: Laptop): " user_input
-        case $user_input in
-            "PC")
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-                create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
-                ;;
-            "1")
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-                create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
-                ;;
-            "Laptop")
-                create_symlink "$HOME/configs/hyprland/hyprland_laptop.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper_laptop.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-                create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
-                ;;
-            "2")
-                create_symlink "$HOME/configs/hyprland/hyprland_laptop.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper_laptop.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-                create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
-                ;;
-            *)
-                create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-                create_symlink "$HOME/configs/hyprland/hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-                create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-                create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
-                ;;
-        esac
+    -c)
+        create_symlink "$HOME/configs/hyprland/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+        create_symlink "$HOME/configs/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+        create_symlink "$HOME/configs/tofi/config" "$HOME/.config/tofi/config"
         ;;
     *)
-        echo "Invalid option: $OPTARG" 1>&2
+        echo "Invalid option: Possibilities are: \n    -c ==> create"
         exit 1
         ;;
 esac
