933185fb9caa435da67047943644793fd95a4d79
matze-isses <ehrhardt.matthis@gmail.com>
2024-11-16 18:52:05 +0100

    working better but not tested at this state
    Signed-off-by: matze-isses <ehrhardt.matthis@gmail.com>

--- dense.py
+++ dense.py
@@ -22,11 +22,11 @@ class Sequence:
 
 
 class PageWrapper:
-    def __init__(self, page_num: int, document: fitz.Document, dpi: int =200):
+    def __init__(self, page_num: int, document: fitz.Document):
         # get data as array
         self.page = document.load_page(page_num)
 
-        pix = self.page.get_pixmap(colorspace=fitz.csRGB, dpi=dpi)
+        pix = self.page.get_pixmap(colorspace=fitz.csRGB, dpi=225)
         img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
         self.array = np.array(img)
         white_pixels = np.all(self.array >= np.array([250, 250, 250]), axis=2)
@@ -52,7 +52,7 @@ class PageWrapper:
         return results
 
 
-def get_bounds(black_lengths, white_lengths, bins=250, percentage_below: float = 0.75, percentage_above: float = 0.1):
+def get_bounds(black_lengths, white_lengths, bins=250, percentage_below: float = 0.6, percentage_above: float = 0.1):
     bin_count = bins
     cap_below = 0
     cap_above = 0
@@ -99,6 +99,10 @@ def process_pdf(pdf_path):
         white_lengths += [seq.length for seq in sequences if not seq.white]
 
     cap_below, cap_above = get_bounds(black_lengths, white_lengths)
+    page_height = 0
+    page_width = 0
+    new_img_np = []
+
 
     for page_num in range(len(doc)):
         page = PageWrapper(page_num, doc)
@@ -118,44 +122,39 @@ def process_pdf(pdf_path):
                 last_black_length = 0
 
         total_removed_rows = sum(- seq.delta for seq in sequences)
-        lengths_to_add_to = sum(seq.length for seq in sequences if seq.delta >= 0 and not seq.white)
+        num_to_add_to = len([seq for seq in sequences if seq.delta >= 0 and seq.white])
 
-        if lengths_to_add_to != 0 and total_removed_rows != 0:
+        if num_to_add_to != 0 and total_removed_rows != 0:
             for seq in sequences:
-                if seq.delta >= 0 and not seq.white:
-                    seq.set_new_length(seq.length + int(total_removed_rows * seq.length / lengths_to_add_to))
+                if seq.delta >= 0 and seq.white:
+                    seq.set_new_length(seq.length + int(total_removed_rows / num_to_add_to))
 
-        new_img_np = []
         for seq in sequences:
             for row in seq._contents.tolist():
                 new_img_np.append(np.array(row))
+        page_height = page.page.rect.height
+        page_width = page.page.rect.width
         
-        new_img_np = np.array(new_img_np)
-        new_img_np = np.append(new_img_np, 255 + np.zeros((page.array.shape[0] - new_img_np.shape[0], new_img_np.shape[1], 3)), axis=0)
-        new_img = Image.fromarray(new_img_np.astype('uint8'), mode='RGB')
+    while len(new_img_np) > page_height:
+        img = np.array(new_img_np[:page_height])
+        new_img_np = new_img_np[page_height:]
+        new_img = Image.fromarray(img.astype('uint8'), mode='RGB')
 
-        # Save the image to a bytes buffer
         img_buffer = BytesIO()
         new_img.save(img_buffer, format='PNG')
         img_buffer.seek(0)
 
-        # Create a new page in the new PDF document with the same dimensions
-        page_width = page.page.rect.width
-        page_height = page.page.rect.height
         new_page = new_doc.new_page(width=page_width, height=page_height)
-
-        # Insert the image into the new page
         new_page.insert_image(fitz.Rect(0, 0, page_width, page_height), stream=img_buffer.getvalue())
 
-    # Save the new PDF
-    output_pdf = pdf_path[:-4] + "_RESHAPE.pdf"
+    output_pdf = pdf_path[:-4] + ".pdf"
     new_doc.save(output_pdf)
     print(f"Processed PDF saved as '{output_pdf}'")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py your_file.pdf")
-        pdf_path = "/home/admin/Downloads/FA2024_Skript.pdf"
+        pdf_path = "/home/admin/Downloads/RelevantGit.pdf"
     else:
         pdf_path = sys.argv[1]
     process_pdf(pdf_path)
